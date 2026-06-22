8f10a5ece374450a743bd79db9b13fda761dccc1
shyamsundar-r <rshyamsundar.in@gmail.com>
2026-06-22 20:22:52 +0530

    Added LastDigit.java

---
+++ LastDigit.java
@@ -0,0 +1,11 @@
+//Return last digit of the given number : 
+import java.util.*;
+public class LastDigit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int result = n%10;
+        System.out.println("Last Digit of the Number is "+result);
+    }
+    
+}

a7fff8003a99ff5b299f8a12159a8de5fdc32989
shyamsundar-r <rshyamsundar.in@gmail.com>
2026-06-22 20:17:35 +0530

    Added IsOdd.java

---
+++ IsOdd.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class IsOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n%2!=0) System.out.println(n+" is a Odd Number");
+        else System.out.println(n+" Not a Odd Number");
+    }   
+}

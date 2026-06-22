d306f64a83068334a27dcefb1cad3230f18e6fde
shyamsundar-r <rshyamsundar.in@gmail.com>
2026-06-22 21:32:54 +0530

    Sum of last digit of two given numbers

--- SumOfLastTwoDigit.java
+++ SumOfLastTwoDigit.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class SumOfLasTwoDigit{
+public class SumOfLastTwoDigit{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
